0eff0e4b776b911ad8ac883acc6c54b27fa2beb2
faithakinbobola <faithakinbobola2@gmail.com>
2024-01-29 07:41:00 -0800

    alx

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""function that finds all multiple of 2 in a list"""
+
+
+def divisible_by_2(my_list-[]):
+    if len(my_list) == 0:
+        return None
+    result_list = []
+    for i in my_list:
+        if i % 2 == 0:
+            my_list.append(True)
+        else:
+            my_list.append(False)
+        return (result_list)
