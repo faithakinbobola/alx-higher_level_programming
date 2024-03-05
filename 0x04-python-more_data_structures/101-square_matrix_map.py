79746864f0d5c8470d84affb958e6293348bb8a6
faithakinbobola <faithakinbobola2@gmail.com>
2024-03-05 08:47:14 -0800

    More data structures:Set, Dictionary

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-
-"""
-A function that computes the square value
-"""
-
-
 def square_matrix_map(matrix=[]):
     return list(map((lambda row: list(map((lambda x: x * x), row))), matrix))
