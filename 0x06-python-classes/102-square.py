1ed88506c473441872632c6caa956c3b3197d4e0
faithakinbobola <faithakinbobola2@gmail.com>
2024-03-22 06:49:08 -0700

    python

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,111 +1,59 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+"""Create a Class Square with size, method of area and getters & setters"""
 
 
 class Square:
-    """Represents a square
+    """Class - Square"""
 
-    Attributes:
-        __size (int): size of a size of the square
-        __position (tuple): position of the square in 2D space
-    """
-    def __init__(self, size=0, position=(0, 0)):
-        """initializes the square
+    def __init__(self, size=0):
+        """Constructor of a Square with the size"""
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
 
-        Args:
-            size (int): size of a side of the square
-            position (tuple): positoin of the square in 2D space
+    def __lt__(self, other):
+        """Compare operator <"""
+        return (self.area() < other.area())
 
-        Returns:
-            None
-        """
-        self.size = size
-        self.position = position
+    def __le__(self, other):
+        """Compare operator <="""
+        return (self.area() <= other.area())
+
+    def __gt__(self, other):
+        """Compare operator >"""
+        return (self.area() > other.area())
+
+    def __ge__(self, other):
+        """Compare operator >="""
+        return (self.area() >= other.area())
+
+    def __eq__(self, other):
+        """Compare operator =="""
+        return (self.area() == other.area())
+
+    def __ne__(self, other):
+        """Compare operator !="""
+        return (self.area() != other.area())
 
     def area(self):
-        """calculates the square's area
-
-        Returns:
-            The area of the square
-        """
-        return (self.__size) ** 2
+        """Method to get the area of the Square"""
+        return (self.__size ** 2)
 
     @property
     def size(self):
-        """getter of __size
-
-        Returns:
-            The size of the square
-        """
-        return self.__size
+        """Getter of the private attribute size"""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """setter of __size
-
-        Args:
-            value (int): size of a side of the square
-
-        Returns:
-            None
-        """
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        else:
-            if value < 0:
-                raise ValueError("size must be >= 0")
+        """Setter for the size private attribute"""
+        if ((type(value) is int) or (type(value) is float)):
+            if (value < 0):
+                raise (ValueError("size must be >= 0"))
             else:
                 self.__size = value
-
-    def my_print(self):
-        """prints the square
-
-        Returns:
-            None
-        """
-        if self.__size == 0:
-            print()
-            return
-        for i in range(self.__position[1]):
-            print()
-        for j in range(self.__size):
-            print("".join([" " for k in range(self.__position[0])]), end="")
-            print("".join(["#" for l in range(self.__size)]))
-
-    @property
-    def position(self):
-        """getter of __position
-
-        Returns:
-            The position of the square in 2D space
-        """
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        """setter of __position
-
-        Args:
-            value (tuple): position of the square in 2D space
-
-        Returns:
-            None
-        """
-        if type(value) is not tuple or len(value) != 2 or \
-           type(value[0]) is not int or value[0] < 0 or \
-           type(value[1]) is not int or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            self.__position = value
-
-    def __str__(self):
-        """String representation of a Square instance
-
-        Returns:
-            Formatted string representing the square
-        """
-        if self.size == 0:
-            return ""
-        string = "\n" * self.position[1] + (" " * self.position[0] +
-                                            "#" * self.size + "\n") * self.size
-        return string[:-1]
+            raise (TypeError("size must be a number"))
